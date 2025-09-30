fe47db816ff1eb302aef48ac9894df202411e3d5
andlybras <andlybras.desenvolvedor@outlook.com>
2025-09-30 16:07:25 -0400

    Realiza a atividade Inversor de Palavras

--- atividades_UmaIntroducaoInformalAoPython/0_chamada_funcoes.py
+++ atividades_UmaIntroducaoInformalAoPython/0_chamada_funcoes.py
@@ -1,6 +1,7 @@
 from calculadora_imc import calculadora_imc
 from conversor_temperatura import conversor_temperatura
 from juros_compostos import juros_compostos
+from inversor_palavras import inversor_de_palavras
 
 def atividades():
 
@@ -10,6 +11,7 @@ def atividades():
         print("\n1 - Calcular IMC")
         print("2 - Converter temperatura")
         print("3 - Calcular juros compostos")
+        print("4 - Inverter palavra")
         escolha = input("\nDigite o número da atividade desejada: ")
 
         try:
@@ -19,6 +21,8 @@ def atividades():
                 conversor_temperatura()
             elif escolha == "3":
                 juros_compostos()
+            elif escolha == "4":
+                inversor_de_palavras()
             else:
                 print("Opção inválida. Tente novamente.")
         except:
---
+++ atividades_UmaIntroducaoInformalAoPython/inversor_palavras.py
@@ -0,0 +1,6 @@
+def inversor_de_palavras():
+
+    palavra = input("\nDigite uma palavra para inverter: ")
+    palavra_invertida = palavra[::-1]
+    
+    print(f"\nA palavra invertida é: {palavra_invertida}")

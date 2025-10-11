eb1c3e630921a08e76ac3c28481bef328d65c33b
andlybras <andlybras.desenvolvedor@outlook.com>
2025-10-11 14:41:30 -0400

    Limpa as entradas fornecidas pelo usuário

--- PyMentor/04 - Guardião de Backups Seletivo/chamada_caminhos.py
+++ PyMentor/04 - Guardião de Backups Seletivo/chamada_caminhos.py
@@ -2,7 +2,11 @@ def chamada_caminhos():
     origem = input("\nDigite o caminho da pasta de origem: ")
     destino = input("Digite o caminho da pasta de destino: ")
 
+    origem = origem.strip().strip('"\'')
+    destino = destino.strip().strip('"\'')
+
     print("\nIniciando o processo de backup...")
+    
     print(f"\nPasta de origem: {origem}")
     print(f"Pasta de destino: {destino}")
 

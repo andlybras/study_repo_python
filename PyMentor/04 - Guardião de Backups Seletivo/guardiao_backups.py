3a1796e18aa1eb1f3af94a0dd8acd11778daf89a
andlybras <andlybras.desenvolvedor@outlook.com>
2025-10-11 14:21:26 -0400

    Implementa o menu do projeto

--- PyMentor/04 - Guardião de Backups Seletivo/guardiao_backups.py
+++ PyMentor/04 - Guardião de Backups Seletivo/guardiao_backups.py
@@ -1,2 +1,24 @@
 import os
 import shutil
+
+extensoes_permitidas = ['.pdf']
+
+def guardiao_backups():
+
+    while True:
+        print("\n" + "~"*30 + " Guardião de Backups  " + "~"*30)
+        print("\nPara iniciar o backup, digite 'Iniciar'")
+        print("Para sair, digite 'Sair'")
+        opcao_menu = input("\nDigite a opção desejada: ")
+
+        if opcao_menu.lower() == 'iniciar':
+            print()
+        
+        elif opcao_menu.lower() == 'sair':
+            print("\nPrograma Encerrado. Até mais!\n")
+            return
+        
+        else:
+            print("\nOpção inválida. Tente novamente.")
+
+guardiao_backups()

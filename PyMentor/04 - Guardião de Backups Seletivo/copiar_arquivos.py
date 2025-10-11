c4307c5ee72eefdf2be0a818193188e8a612231e
andlybras <andlybras.desenvolvedor@outlook.com>
2025-10-11 16:45:06 -0400

    Complementa o planejamento para a construção do código

--- PyMentor/04 - Guardião de Backups Seletivo/copiar_arquivos.py
+++ PyMentor/04 - Guardião de Backups Seletivo/copiar_arquivos.py
@@ -1,2 +1,14 @@
 def copiar_arquivos():
     print("Função de copiar arquivos.")
+
+    # Abra o arquivo log.txt em modo de escrita
+
+    # Vai criar dentro da pasta de origem, a pasta de backup datata
+        # adcione a informação no log
+
+    # uma lista com os caminhos dentro da pasta de origem deve ser gerada, aceitando somente arquivos
+
+    # para cada caminho da lista, separar o nome da extensão e armazenar em variáveis
+
+        # se a extensão estiver na lista de extensões permitidas, copie o arquivo para a pasta do backup datata
+            # adcione a informação no log
--- PyMentor/04 - Guardião de Backups Seletivo/mover_arquivos.py
+++ PyMentor/04 - Guardião de Backups Seletivo/mover_arquivos.py
@@ -1,2 +1,14 @@
 def mover_arquivos():
     print("Função de mover arquivos.")
+
+    # Abra o arquivo log.txt em modo de escrita
+
+    # Vai criar dentro da pasta de origem, a pasta de backup datata
+        # adcione a informação no log
+
+    # uma lista com os caminhos dentro da pasta de origem deve ser gerada, aceitando somente arquivos
+
+    # para cada caminho da lista, separar o nome da extensão e armazenar em variáveis
+
+        # se a extensão estiver na lista de extensões permitidas, mova o arquivo para a pasta do backup datata
+            # adcione a informação no log

94af4cb6691f89c0a514691d46c67e4b838a11c7
andlybras <andlybras.desenvolvedor@outlook.com>
2025-10-13 15:11:42 -0400

    Refatora a chamada de caminhos para aceitar somente entradas válidas

--- PyMentor/04 - Guardião de Backups Seletivo/chamada_caminhos.py
+++ PyMentor/04 - Guardião de Backups Seletivo/chamada_caminhos.py
@@ -1,12 +1,26 @@
+import os
+
 def chamada_caminhos():
-    origem = input("\nDigite o caminho da pasta de origem: ")
-    destino = input("Digite o caminho da pasta de destino: ")
 
-    origem = origem.strip().strip('"\'')
-    destino = destino.strip().strip('"\'')
+    while True:
+        origem = input("\nDigite o caminho da pasta de origem: ").strip().strip('"\'')
 
-    print("\nIniciando o processo de backup...")
-    
+        if os.path.isdir(origem):
+            break
+
+        else:
+            print(f"ERRO: A pasta de origem '{origem}' não foi encontrada. Tente novamente.")
+
+    while True:
+        destino = input("Digite o caminho da pasta de destino: ").strip().strip('"\'')
+
+        if os.path.isdir(destino):
+            break
+        
+        else:
+            print(f"ERRO: A pasta de destino '{destino}' não foi encontrada. Tente novamente.")
+
+    print("\nCaminhos validados. Iniciando o processo de backup...")
     print(f"\nPasta de origem: {origem}")
     print(f"Pasta de destino: {destino}")
 

1252668428e84a4e66ec68350f5924dee79f2d69
Andly <andlybras.desenvolvedor@outlook.com>
2025-10-14 08:56:29 -0400

    Refatora a fase 1 para tornar o menu mais dinâmico

--- PyMentor/06 - Câmbio-CLI Terminal de Cotações/conversor_cambio.py
+++ PyMentor/06 - Câmbio-CLI Terminal de Cotações/conversor_cambio.py
@@ -1,37 +1,33 @@
 def coversor_cambio():
 
-    print("\n" + "~"*30 + " Conversor de Câmbio " + "~"*30)
-    print("\nEscolha a moeda que você deseja converter:")
-    print("\n1. Dólar (USD)")
-    print("2. Euro (EUR)")
-    print("3. Libra Esterlina (GBP)")
-    print("4. Iene Japonês (JPY)")
-
-    escolha = input("\nDigite o número da moeda (1-4): ")
-
-    if escolha not in ['1', '2', '3', '4']:
-        print("Opção inválida. Por favor, escolha um número entre 1 e 4.")
-        return
-
-    valor = float(input("Digite o valor que você deseja converter: "))
-
     taxas_cambio = {
-        '1': 5.25,
-        '2': 6.20,
-        '3': 7.30,
-        '4': 0.048,
+        '1':  [5.25, 'USD'],
+        '2':  [6.20, 'EUR'],
+        '3':  [7.30, 'GBP'],
+        '4':  [0.048, 'JPY'],
     }
 
-    taxa = taxas_cambio[escolha]
-    valor_convertido = valor * taxa
+    print("\n" + "~"*30 + " Conversor de Câmbio " + "~"*30)
+    print("\nEscolha a moeda que você deseja converter:")
 
-    moedas = {
-        '1': 'USD',
-        '2': 'EUR',
-        '3': 'GBP',
-        '4': 'JPY'}
-    
-    print(f"{valor} {moedas[escolha]} é equivalente a R$ {valor_convertido:.2f}")
+    for escolha, taxa_moeda in taxas_cambio.items():
+        for taxa, moeda in [taxa_moeda]:
+            print(f"{escolha}. {moeda} - Taxa de câmbio: R$ {taxa}")
+
+    escolha = input("\nDigite sua opção: ")
+
+    while True:
+
+        if escolha not in taxas_cambio.keys():
+            print("Opção inválida.")
+            break
+        
+        else:
+            valor = float(input("Digite o valor que você deseja converter: "))
+            taxa = taxas_cambio[escolha][0]
+            valor_convertido = valor * taxa        
+            print(f"{valor} {taxas_cambio[escolha][1]} é equivalente a R$ {valor_convertido:.2f}")
+            break
 
 if __name__ == "__main__":
     coversor_cambio()
